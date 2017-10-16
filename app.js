8915c0dd40a41ae7ab2c8d7fd08fb2086dd4e5d6
JoeGio <jaturon.kumo@gmail.com>
2017-10-16 23:14:52 +0700

    fix es6 and readme

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ var app  =  express();
 
 app.set("view engine", "ejs");
 
-app.get("/", function(req,res){
+app.get("/", (req,res) => {
     res.render("home");
 });
 
-app.get("/about", function(req,res){
+app.get("/about", (req,res) => {
     res.render("about");
 });
 
-app.listen(process.env.PORT ||3000, function(){
+app.listen(process.env.PORT ||3000, () => {
     console.log("Server has started");
 });
